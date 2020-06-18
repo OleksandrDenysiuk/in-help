da132ba431e4add6027084d0025bfba8a2dd76d5
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-06-18 14:56:48 +0200

    Initial Commit

---
+++ src/main/java/com/portfolio/inhelp/InHelpApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.inhelp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InHelpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InHelpApplication.class, args);
+    }
+
+}
