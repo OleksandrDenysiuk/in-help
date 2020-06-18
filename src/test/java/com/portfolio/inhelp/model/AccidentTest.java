a223cc3efdfe4b01b725e9a79585598554dc25cc
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-06-18 18:31:20 +0200

    Add tests

---
+++ src/test/java/com/portfolio/inhelp/model/AccidentTest.java
@@ -0,0 +1,64 @@
+package com.portfolio.inhelp.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.NoSuchElementException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccidentTest {
+    Accident accident;
+
+    @BeforeEach
+    void setUp() {
+        accident = Accident.builder().id(1L).images(new HashSet<>()).helpers(new HashSet<>()).build();
+    }
+
+    @Test
+    void addImage() {
+        Image imageToAdd = Image.builder().id(1L).build();
+        accident.addImage(imageToAdd);
+
+        assertEquals(1, accident.getImages().size());
+        assertNotNull(accident.getImages().stream().filter(image -> image.getId().equals(1L)).findFirst().get());
+    }
+
+    @Test
+    void removeImage() {
+        Image imageToRemove = Image.builder().id(1L).build();
+        accident.addImage(imageToRemove);
+
+        accident.removeImage(imageToRemove);
+
+        assertNotEquals(1, accident.getImages().size());
+        assertThrows(NoSuchElementException.class, () -> {
+           accident.getImages().stream().filter(image -> image.getId().equals(1L)).findFirst().get();
+        });
+    }
+
+    @Test
+    void addHelper() {
+        User helperToAdd = User.builder().id(1L).accidents(new HashSet<>()).build();
+        accident.addHelper(helperToAdd);
+
+
+        assertEquals(1, accident.getHelpers().size());
+        assertNotNull(accident.getHelpers().stream().filter(helper -> helper.getId().equals(1L)).findFirst().get());
+    }
+
+    @Test
+    void removeHelper() {
+        User helperToRemove = User.builder().id(1L).accidents(new HashSet<>()).build();
+
+        accident.addHelper(helperToRemove);
+
+        accident.removeHelper(helperToRemove);
+
+        assertNotEquals(1, accident.getHelpers().size());
+        assertThrows(NoSuchElementException.class, () -> {
+            accident.getHelpers().stream().filter(helper -> helper.getId().equals(1L)).findFirst().get();
+        });
+    }
+}
