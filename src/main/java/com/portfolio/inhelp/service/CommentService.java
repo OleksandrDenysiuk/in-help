fbb4057dc03b0386fc309d4df8640f5524132aef
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-06-19 20:59:54 +0200

    Refactor

--- src/main/java/com/portfolio/inhelp/service/CommentService.java
+++ src/main/java/com/portfolio/inhelp/service/CommentService.java
@@ -1,13 +1,20 @@
 package com.portfolio.inhelp.service;
 
+import com.portfolio.inhelp.command.CommentCommand;
 import com.portfolio.inhelp.dto.CommentDto;
 
-import java.util.Set;
+import java.util.List;
 
 public interface CommentService {
-    CommentDto getOne(Long commentId);
-    Set<CommentDto> getAll();
-    CommentDto create(CommentDto accidentCommand);
-    CommentDto update(CommentDto accidentCommand);
-    void delete(Long commentId);
+    List<CommentDto> getAllByAccidentId(Long accidentId);
+    List<CommentDto> getAllByNewsId(Long newsId);
+
+    CommentDto create(CommentCommand commentCommand, Long accidentId, Long authorId);
+    CommentDto create(CommentCommand commentCommand, Long accidentId, Long newsId, Long authorId);
+
+    CommentDto update(CommentCommand commentCommand, Long accidentId, Long authorId);
+    CommentDto update(CommentCommand commentCommand, Long accidentId, Long newsId, Long authorId);
+
+    void delete(Long commentId, Long accidentId, Long authorId);
+    void delete(Long commentId, Long accidentId, Long newsId, Long authorId);
 }
