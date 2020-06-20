f4c1815292549d87488a3f94a80ef9c5e8aec2b2
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-06-20 21:17:40 +0200

    Fix bug

--- src/main/java/com/portfolio/inhelp/service/NewsService.java
+++ src/main/java/com/portfolio/inhelp/service/NewsService.java
@@ -1,5 +1,6 @@
 package com.portfolio.inhelp.service;
 
+import com.portfolio.inhelp.command.NewsCommand;
 import com.portfolio.inhelp.dto.NewsDto;
 
 import java.util.List;
@@ -8,7 +9,7 @@ public interface NewsService {
     NewsDto getOne(Long newsId);
     List<NewsDto> getAll();
     List<NewsDto> getAllByAccidentId(Long accidentId);
-    NewsDto create(NewsDto accidentCommand, Long accidentId, Long userId);
-    NewsDto update(NewsDto accidentCommand, Long accidentId, Long userId);
+    NewsDto create(NewsCommand newsCommand, Long accidentId, Long userId);
+    NewsDto update(NewsCommand newsCommand, Long accidentId, Long userId);
     void delete(Long newsId, Long accidentId, Long userId);
 }
