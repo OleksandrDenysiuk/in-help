15b270b1c2f1651130f6605706d3dc96f6938f80
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-06-20 19:15:00 +0200

    Add Image repository

---
+++ src/main/java/com/portfolio/inhelp/repository/ImageRepository.java
@@ -0,0 +1,7 @@
+package com.portfolio.inhelp.repository;
+
+import com.portfolio.inhelp.model.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ImageRepository extends JpaRepository<Image, Long> {
+}
