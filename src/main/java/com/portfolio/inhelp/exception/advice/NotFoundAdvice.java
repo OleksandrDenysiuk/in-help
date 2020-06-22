8b83d77ef596ee7e0e537f9dcff755f81a933bd6
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-06-22 22:42:21 +0200

    Add NumberFormatAdvice class

--- src/main/java/com/portfolio/inhelp/exception/advice/NotFoundAdvice.java
+++ src/main/java/com/portfolio/inhelp/exception/advice/NotFoundAdvice.java
@@ -1,5 +1,6 @@
-package com.portfolio.inhelp.exception;
+package com.portfolio.inhelp.exception.advice;
 
+import com.portfolio.inhelp.exception.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
