0ebf12fbc6c53eeee7d7e60151e77afd0bb59306
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-06-22 20:22:29 +0200

    Delete method

--- src/main/java/com/portfolio/inhelp/repository/AccidentRepository.java
+++ src/main/java/com/portfolio/inhelp/repository/AccidentRepository.java
@@ -6,7 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface AccidentRepository extends JpaRepository<Accident, Long> {
-    Accident findByIdAndUserId(Long id, Long userId);
-
     List<Accident> findAllByUserId(Long userId);
 }
