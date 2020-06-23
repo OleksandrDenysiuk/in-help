d6dfb0fd33c9caa69d51a866f948d6e60be94e79
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-06-23 21:08:23 +0200

    Add RegistrationController

---
+++ src/main/java/com/portfolio/inhelp/controller/api/RegistrationController.java
@@ -0,0 +1,32 @@
+package com.portfolio.inhelp.controller.api;
+
+import com.portfolio.inhelp.command.UserCommand;
+import com.portfolio.inhelp.dto.UserDto;
+import com.portfolio.inhelp.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/api")
+public class RegistrationController {
+
+    private final UserService userService;
+
+    public RegistrationController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    @ResponseBody
+    public UserDto registerUserAccount(@RequestBody UserCommand userCommand){
+        UserDto userDto = userService.getOneByUsername(userCommand.getUsername());
+        if(userDto != null){
+            throw  new RuntimeException("User exists");
+        }else {
+            return userService.create(userCommand);
+        }
+    }
+}
