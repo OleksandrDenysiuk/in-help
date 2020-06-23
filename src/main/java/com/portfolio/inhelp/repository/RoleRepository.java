926f4d534bc3b9b37eb3c6709a7172a9030d9fe8
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-06-23 20:32:49 +0200

    Add Role repository

---
+++ src/main/java/com/portfolio/inhelp/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.portfolio.inhelp.repository;
+
+import com.portfolio.inhelp.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByName(String name);
+}
