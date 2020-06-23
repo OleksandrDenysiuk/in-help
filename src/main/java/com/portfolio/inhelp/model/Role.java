8728f95bb3f0819cec7d7d40fa257584542b417d
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-06-23 20:32:33 +0200

    Add Role entity

---
+++ src/main/java/com/portfolio/inhelp/model/Role.java
@@ -0,0 +1,29 @@
+package com.portfolio.inhelp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "Role")
+@Table(name = "user_roles")
+public class Role implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Override
+    public String getAuthority() {
+        return name;
+    }
+}
