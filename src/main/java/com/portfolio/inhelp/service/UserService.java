915954902432d7fb822f04b6c8072d18c0534da1
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-06-23 20:37:12 +0200

    Add extending to UserDetailsService

--- src/main/java/com/portfolio/inhelp/service/UserService.java
+++ src/main/java/com/portfolio/inhelp/service/UserService.java
@@ -2,12 +2,14 @@ package com.portfolio.inhelp.service;
 
 import com.portfolio.inhelp.command.UserCommand;
 import com.portfolio.inhelp.dto.UserDto;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 import java.util.List;
 
-public interface UserService {
+public interface UserService extends UserDetailsService {
     UserDto getOne(Long id);
     List<UserDto> getAll();
+    UserDto getOneByUsername(String username);
     UserDto create(UserCommand userCommand);
     UserDto update(UserCommand userCommand);
     void delete(Long id);
