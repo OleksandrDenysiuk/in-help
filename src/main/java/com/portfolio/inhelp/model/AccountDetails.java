ff058b58ff57a0adbc0c6ebd8428332c47a875bc
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-06-24 16:29:36 +0200

    Add AccountDetails class

---
+++ src/main/java/com/portfolio/inhelp/model/AccountDetails.java
@@ -0,0 +1,56 @@
+package com.portfolio.inhelp.model;
+
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AccountDetails implements UserDetails {
+
+    private String username;
+    private String password;
+    Set<Role> roles = new HashSet<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
