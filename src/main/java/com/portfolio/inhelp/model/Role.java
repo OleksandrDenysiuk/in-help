46693b8864fc706210466a95eebab1b0ff0f352a
OleksandrDenysiuk <denysiuk.oleksandr1997@gmail.com>
2020-06-24 16:30:19 +0200

    Add Builder annotation

--- src/main/java/com/portfolio/inhelp/model/Role.java
+++ src/main/java/com/portfolio/inhelp/model/Role.java
@@ -1,9 +1,6 @@
 package com.portfolio.inhelp.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 
 import javax.persistence.*;
@@ -12,6 +9,7 @@ import javax.persistence.*;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @Entity(name = "Role")
 @Table(name = "user_roles")
 public class Role implements GrantedAuthority {
